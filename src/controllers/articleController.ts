d6bedcdcf312f8d36ade9f33907d726a2e436f25
Modebe Stephen <modebestephen2@gmail.com>
2023-01-19 10:46:05 +0100

    Refactor article controller

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -19,8 +19,8 @@ export const findByID = async (
   req: express.Request,
   res: express.Response
 ): Promise<void> => {
-  const id = parseInt(req.params.id);
   try {
+    const id = req.params.id;
     const article = await store.show(id);
     res.status(200).json(article);
   } catch (error) {
@@ -32,13 +32,13 @@ export const createArticle = async (
   req: express.Request,
   res: express.Response
 ): Promise<void> => {
-  const article: Article = {
-    title: 'RESTful API',
-    content:
-      'RESTful API means Representational State Transfer(ful) Application Interface. RESTful API provides a well-organized, industry standard and predictable way to structure our API endpoint'
-  };
-  const new_article = await store.create(article);
   try {
+    const {title, content} = req.body;
+    const article: Article = {
+      title: title,
+      content: content
+    };
+    const new_article = await store.create(article);
     res.status(201).json(new_article);
   } catch (error) {
     res.status(400).json(error);
@@ -49,8 +49,8 @@ export const deleteArticle = async (
   req: express.Request,
   res: express.Response
 ): Promise<void> => {
-  const id = parseInt(req.params.id);
   try {
+    const id = req.params.id;
     const deletedArticle = await store.delete(id);
     res.status(200).json(deletedArticle);
   } catch (error) {
